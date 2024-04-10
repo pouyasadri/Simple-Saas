2038c2622c45277baf453d60b2016ff7538992be
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-11 00:10:36 +0200

    Define transactions table fields in its migration file ( 2024_04_10_215852_create_transactions_table.php )

--- database/migrations/2024_04_10_215852_create_transactions_table.php
+++ database/migrations/2024_04_10_215852_create_transactions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->string('status');
+            $table->decimal('price', 10, 2);
+            $table->integer('credits');
+            $table->string('session_id');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('package_id')->constrained('packages');
             $table->timestamps();
         });
     }
