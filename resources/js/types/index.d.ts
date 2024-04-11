3ed3506797cf8b8f5c7b984d8bd1b2884b6be7cf
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-11 23:59:37 +0200

    add the available_credits to user default type and also create FeatureType based on FeatureResource.php

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -3,6 +3,17 @@ export interface User {
     name: string;
     email: string;
     email_verified_at: string;
+    available_credits: number;
+}
+
+export interface FeatureType {
+    id: number;
+    image: string;
+    name: string;
+    route_name: string;
+    is_active: boolean;
+    description: string;
+    required_credits: number;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
