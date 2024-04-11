0069c434a06476780290dddb348eac746d1a2e72
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-11 15:05:53 +0200

    add database Seeder data to create a user, create two features and create three packages in DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feature;
+use App\Models\Package;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +20,42 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        Feature::create([
+            'image' => 'https://via.placeholder.com/150',
+            'route_name' => 'feature1.index',
+            'name' => 'Calculate Sum',
+            'description' => 'Calculate the sum of two numbers',
+            'required_credits' => 1,
+            'is_active' => true,
+        ]);
+        Feature::create([
+            'image' => 'https://via.placeholder.com/150',
+            'route_name' => 'feature2.index',
+            'name' => 'Calculate Difference',
+            'description' => 'Calculate the difference of two numbers',
+            'required_credits' => 3,
+            'is_active' => true,
+        ]);
+
+        Package::create([
+            'name' => 'Basic',
+            'price' => 5,
+            'credits' => 20,
+        ]);
+
+        Package::create([
+            'name' => 'Silver',
+            'price' => 20,
+            'credits' => 100,
+        ]);
+
+        Package::create([
+            'name' => 'Gold',
+            'price' => 50,
+            'credits' => 500,
         ]);
     }
 }
