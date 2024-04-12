d6f9c0ef71c141d19196cefee30c2ce2b9c03e59
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-12 03:04:57 +0200

    moving the `feature1.index`, `feature1.calculate`, `feature2.index`, `feature1.calculate` to the group for auth middleware to increase the security of the app and also create `credit.index`, `credit.success`, `credit.cancel`, `credit.buy` inside this auth middleware and create `credit.webhook` post route to process the webhook from stripe.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CreditController;
 use App\Http\Controllers\Feature1Controller;
 use App\Http\Controllers\Feature2Controller;
 use App\Http\Controllers\ProfileController;
@@ -20,14 +21,22 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::post('/buy-credits/webhook', [CreditController::class, 'webhook'])->name('credit.webhook');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/feature1', [Feature1Controller::class, 'index'])->name('feature1.index');
+    Route::post('/feature1/calculate', [Feature1Controller::class, 'calculate'])->name('feature1.calculate');
+    Route::get('/feature2', [Feature2Controller::class, 'index'])->name('feature2.index');
+    Route::post('/feature2/calculate', [Feature2Controller::class, 'calculate'])->name('feature2.calculate');
+
+    Route::get('buy-credits', [CreditController::class, 'index'])->name('credit.index');
+    Route::get('buy-credits/success', [CreditController::class, 'success'])->name('credit.success');
+    Route::get('buy-credits/cancel', [CreditController::class, 'cancel'])->name('credit.cancel');
+    Route::post('buy-credits/{package}', [CreditController::class, 'buyCredits'])->name('credit.buy');
 });
 
-Route::get('/feature1', [Feature1Controller::class, 'index'])->name('feature1.index');
-Route::post('/feature1/calculate', [Feature1Controller::class, 'calculate'])->name('feature1.calculate');
-Route::get('/feature2', [Feature2Controller::class, 'index'])->name('feature2.index');
-Route::post('/feature2/calculate', [Feature2Controller::class, 'calculate'])->name('feature2.calculate');
+
 require __DIR__ . '/auth.php';
